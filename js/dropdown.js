ed59506eec5c3b5078541a6811db3f1475792bf3
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2024-06-11 19:13:37 +0600

    Fixed

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,4 +1,4 @@
-document.querySelector('.dropdown__button').addEventListener('click', function(){
+    document.querySelector('.dropdown__button').addEventListener('click', function(){
     document.querySelector('.dropdown__list').classList.toggle('dropdown__list--visible');
     
 });
--- js/dropdown_lang.js
+++ js/dropdown_lang.js
@@ -11,10 +11,34 @@ document.querySelectorAll('.dropdown__list-item__lang').forEach(function(listIte
         document.querySelector('.dropdown__input-hidden').value = this.dataset.value;
         document.querySelector('.dropdown__list__lang').classList.remove('dropdown__list__lang--visible');
     })
-})
+});
 
 document.addEventListener('click', function(e){
     if (e.target !== document.querySelector('.dropdown__button__lang')){
         document.querySelector('.dropdown__list__lang').classList.remove('dropdown__list__lang--visible');
     }
-})
+});
+
+
+document.querySelector('.dropdown__button__lang_mobile').addEventListener('click', function(){
+    document.querySelector('.dropdown__list__lang_mobile').classList.toggle('dropdown__list__lang_mobile--visible');
+    
+});
+
+document.querySelectorAll('.dropdown__list-item__lang').forEach(function(listItem){
+    listItem.addEventListener('click', function(e){
+        e.stopPropagation();
+        document.querySelector('.dropdown__button__lang_mobile').innerText = this.innerText;
+        document.querySelector('.dropdown__button__lang_mobile').focus();
+        document.querySelector('.dropdown__input-hidden').value = this.dataset.value;
+        document.querySelector('.dropdown__list__lang_mobile').classList.remove('dropdown__list__lang_mobile--visible');
+    });
+});
+
+document.addEventListener('click', function(e){
+    if (e.target !== document.querySelector('.dropdown__button__lang_mobile')){
+        document.querySelector('.dropdown__list__lang_mobile').classList.remove('dropdown__list__lang_mobile--visible');
+    }
+});
+
+
